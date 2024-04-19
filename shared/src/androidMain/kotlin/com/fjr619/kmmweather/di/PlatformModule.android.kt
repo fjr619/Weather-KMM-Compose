284b219083462911e15c700e4a6c3adb4539780c
Franky Wijanarko <frank.jr.619@gmail.com>
2024-04-19 10:59:56 +0700

    Revert "expect and actual for viewModelDefinition"
    This reverts commit 53b92e54464aeb3b9a2a5eb9d25f4640fa5a37d7.

--- shared/src/androidMain/kotlin/com/fjr619/kmmweather/di/PlatformModule.android.kt
+++ shared/src/androidMain/kotlin/com/fjr619/kmmweather/di/PlatformModule.android.kt
@@ -7,15 +7,10 @@ import com.fjr619.kmmweather.data.local.DataStoreProviderImpl
 import com.fjr619.kmmweather.data.local.datastore.DataStoreProvider
 import com.fjr619.kmmweather.domain.location.AndroidLocationService
 import com.fjr619.kmmweather.domain.location.LocationService
-import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import io.ktor.client.engine.android.Android
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
-import org.koin.core.qualifier.Qualifier
 import org.koin.dsl.module
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.definition.Definition
-import org.koin.core.definition.KoinDefinition
 
 actual fun platformModule(): Module = module {
     single<DataStoreProvider> { DataStoreProviderImpl(androidContext()) }
@@ -27,7 +22,3 @@ actual fun platformModule(): Module = module {
     single { Android.create() }
     factory <LocationService> { AndroidLocationService(context = androidContext()) }
 }
-actual inline fun <reified T : ViewModel> Module.viewModelDefinition(
-    qualifier: Qualifier?,
-    noinline definition: Definition<T>,
-): KoinDefinition<T> = viewModel(qualifier = qualifier, definition = definition)
--- shared/src/commonMain/kotlin/com/fjr619/kmmweather/di/PlatformModule.kt
+++ shared/src/commonMain/kotlin/com/fjr619/kmmweather/di/PlatformModule.kt
@@ -2,13 +2,6 @@ package com.fjr619.kmmweather.di
 
 import com.fjr619.kmmweather.data.local.datastore.DataStoreProvider
 import org.koin.core.module.Module
-import dev.icerock.moko.mvvm.viewmodel.ViewModel
-import org.koin.core.definition.Definition
-import org.koin.core.definition.KoinDefinition
-import org.koin.core.qualifier.Qualifier
+
 expect fun platformModule(): Module
 
-expect inline fun <reified T : ViewModel> Module.viewModelDefinition(
-    qualifier: Qualifier? = null,
-    noinline definition: Definition<T>
-): KoinDefinition<T>
